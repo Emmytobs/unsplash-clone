f699a112830b4edcebbc5ee9045ebf142cea8063
Emmytobs <Eotobo11@gmail.com>
2021-03-16 13:26:08 +0100

    Added link to my github

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ import "./styles.css";
 export default function Header(props) {
   return (
     <header className="header">
+      <p className="author">Created by <a href="https://github.com/emmytobs">Emmytobs</a></p>
       <InputBox
         input={props.input}
         handleChange={props.handleChange}
